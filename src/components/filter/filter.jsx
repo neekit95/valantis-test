d909aa10349d214311206c9fdefe86375d9cb29b
neekit95 <nikita-ribalkin@mail.ru>
2024-03-08 14:23:09 +0300

    filter ui + toggle

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import style from './filter.module.scss'
+import CloseIcon from '@mui/icons-material/Close';
 
-const Filter = () => {
+const Filter = (props) => {
+	function filter(ids, param) {
+		console.log(`$ids.length: ${ids.length}`);
+	}
+
+	useEffect(() => {
+		filter(props.allIds);
+	}, [props.allIds]);
 	return (
-		<div>
-			filter
+		<div className={style.container}>
+
+			<div className={style.buttonContainer}>
+				<button
+					className={style.button}
+					onClick={props.onToggle}
+				>
+					<CloseIcon/>
+				</button>
+			</div>
+
+			<div className={style.content}>
+
+				<div className={style.contentInput}>
+
+					<label htmlFor="product">
+						Название
+						<input name={'product'} placeholder={'Введите название товара'}/>
+					</label>
+				</div>
+
+				<div className={style.contentInput}>
+					<label htmlFor="price">
+						Бренд
+						<input name={'price'} placeholder={'Введите название бренда'}/>
+					</label>
+				</div>
+
+				<div className={style.contentInput}>
+					<label htmlFor="price">
+						Цена
+						<input name={'price'} placeholder={'Введите цену товара'}/>
+					</label>
+				</div>
+
+				<div className={style.contentInput}>
+					<label htmlFor="id">
+						Id
+						<input name={'id'} placeholder={'Введите id товара'}/>
+					</label>
+				</div>
+
+				<button> Применить</button>
+			</div>
+
 		</div>
-	);
+	)
+
 };
 
 export default Filter;
--- src/views/homepage2/homepage2.jsx
+++ src/views/homepage2/homepage2.jsx
@@ -1,9 +1,11 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, {useEffect, useState, useRef, Fragment} from 'react';
 import style from './homepage2.module.scss'
 import md5 from "md5";
 import Card from '../../components/card/card'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Filter from "../../components/filter/filter";
+import TuneIcon from '@mui/icons-material/Tune';
 
 const Homepage2 = () => {
 	const [listOfID, setListOfID] = useState([]);
@@ -14,14 +16,16 @@ const Homepage2 = () => {
 	const [page, setPage] = useState(1);
 	let URL = 'http://api.valantis.store:40000/';
 	let URL2 = 'https://api.valantis.store:41000/';
-	const [textForUser, setTextForUser] = useState('Загрузка...');
+	const [textForUser, setTextForUser] = useState(`Загрузка`);
 	const [allIds, setAllIds] = useState([]);
 	// const [isFirstRender, setIsFirstRender] = useState(true);
 	const [begin, setBegin] = useState(0);
 	const [end, setEnd] = useState(50);
 	const [currentIds, setCurrentIds] = useState([]);
 	const [isFirstRender, setIsFirstRender] = useState(true);
-	const [isHided, setIsHided] = useState(true);
+	const [isHidedAdmin, setIsHidedAdmin] = useState(true);
+	const [isHidedFilter, setIsHidedFilter] = useState(false);
+	const [filteredItems, setFilteredItems] = useState([]);
 
 
 	const getCurrentDate = () => {
@@ -33,7 +37,6 @@ const Homepage2 = () => {
 	};
 
 	const fetchIdsFromServer = async () => {
-		// console.clear();
 		setIsLoading(true);
 		const maxAttempts = 10;
 		for (let attempt = 1; attempt <= maxAttempts; attempt++) {
@@ -45,8 +48,6 @@ const Homepage2 = () => {
 				const timestamp = getCurrentDate();
 				const password = 'Valantis';
 				const xAuth = md5(`${password}_${timestamp}`);
-
-				console.log(`загружаем ids (попытка ${attempt})`);
 
 				const idsResponse = await fetch(URL2, {
 					method: 'POST',
@@ -64,17 +65,10 @@ const Homepage2 = () => {
 				});
 
 				const idsData = await idsResponse.json();
-
 				const ids = idsData.result;
-				console.log(`количество ID при попытке № ${attempt}: ${ids.length}`);
-
 				const uniqueIds = [...new Set(ids)]
-				console.log(`Количество уникальных ID при попытке № ${attempt}:`, uniqueIds.length);
-
 				setListOfID(uniqueIds);
-
 				break;
-
 			} catch (error) {
 				console.error(`Error on attempt ${attempt}:`, error);
 				if (attempt === maxAttempts) {
@@ -156,9 +150,7 @@ const Homepage2 = () => {
 
 				const idsData = await idsResponse.json();
 				const ids = idsData.result;
-				console.log(`Всего  id на сервере: ${ids.length}`);
 				const uniqueAllIds = Array.from(new Set(ids));
-				console.log(`Всего уникальных id на сервере: ${uniqueAllIds.length}`);
 				setAllIds(uniqueAllIds);
 				break;
 			} catch (error) {
@@ -190,7 +182,6 @@ const Homepage2 = () => {
 		await setBegin(prevBegin => prevBegin - 50);
 		await setEnd(prevEnd => prevEnd - 50);
 		await setPage(prevPage => prevPage - 1);
-		// setIsLoading(false);
 	}
 
 
@@ -205,114 +196,137 @@ const Homepage2 = () => {
 		}
 	};
 
+	// При isLoading отображается "Загрузка..."
 	useEffect(() => {
-		setTextForUser('Загрузка...');
+		setTextForUser(`Загрузка... `);
 	}, [isLoading]);
 
+	// При монтировании компонента вызываем tryFetch();
 	useEffect(() => {
 		tryFetch()
 	}, []);
 
+	// При изменении begin, end, allIds срезаем allIds от begin до end
 	useEffect(() => {
-		console.log('allIds', allIds.length)
 		setCurrentIds(allIds.slice(begin, end));
 	}, [allIds, begin, end]);
 
+	// При изменении page, если это не firstRender получаем актуальные элементы
 	useEffect(() => {
 		if (!isFirstRender) {
 			getCurrentItems(begin, end);
 		}
-		console.log('begin', begin);
-		console.log('end', end);
 	}, [page]);
 
-	useEffect(() => {
-		console.log('currentIds', currentIds.length)
-	}, [currentIds]);
-
-	useEffect(() => {
-		console.log('listOfItems', listOfItems)
-	}, [listOfID, listOfItems]);
-
+	// Когда кол-во элементов в массиве listOfID = 50, вызываем fetchItemsFromServer, затем вызов getAllIds (это нужно для производительности, затем мы не будем использовать listOfID)
 	useEffect(() => {
 		if (listOfID.length === 50) {
 			fetchItemsFromServer(listOfID).then(() => {
 					setIsLoading(false);
 					getAllIds();
-				// if (isLoading === false) {
-				// 	getAllIds();
-				// }
 				}
 			);
 		}
 	}, [listOfID.length !== 0]);
 
 
-	function setHide () {
-		setIsHided(!isHided);
+
+	function setHideAdmin() {
+		setIsHidedAdmin(!isHidedAdmin);
+	}
+
+	function setHideFilter() {
+		setIsHidedFilter(!isHidedFilter);
 	}
 
 
 	return (
-		<div className={style.homepage}>
-			<div
+		<div className={style.layout}>
 
-				onClick={setHide}
-			>
-				{isHided ?
-					<div className={style.adminPanelHided}>
-						<p> + </p>
-					</div>
+			<div className={style.filter}>
+				{isHidedFilter ?
+					<Filter
+						onToggle={setHideFilter}
+						// onFilter={getCurrentItems}
+						allIds={allIds}
+						isLoading={isLoading}
+						isFirstRender={isFirstRender}
+					/>
 					:
-					<div className={style.adminPanel}>
-						<p> page:<span>{page} </span></p>
-						<p> listOfItems.length: <span>{listOfItems.length}</span></p>
-						<p> listOfID.length: <span> {listOfID.length}</span></p>
-						<p> allIds.length:<span>{allIds.length}  </span></p>
-						<p> Элементы: {begin} - {end}</p>
-					</div>
-
-				}
-
-			</div>
-
-			<div className={style.container}>
-				{isLoading ? <div> {textForUser} </div> :
-
-					<div className={style.cards}>
-						{listOfItems.map((item, index) => (
-							<div key={index}>
-								<Card items={item}/>
-							</div>
-						))}
-					</div>
+					<button
+						className={style.filterBtn}
+						onClick={setHideFilter}>
+						Открыть фильтр
+						<TuneIcon/>
+					</button>
 				}
 			</div>
 
-			<div className={style.buttons}>
+
+			<div className={style.homepage}>
+
+
+				<div
+
+					onClick={setHideAdmin}
+				>
+					{isHidedAdmin ?
+						<div className={style.adminPanelHided}>
+							<p> + </p>
+						</div>
+						:
+						<div className={style.adminPanel}>
+							<p> page:<span>{page} </span></p>
+							<p> listOfItems.length: <span>{listOfItems.length}</span></p>
+							<p> listOfID.length: <span> {listOfID.length}</span></p>
+							<p> allIds.length:<span>{allIds.length}  </span></p>
+							<p> Элементы: {begin} - {end}</p>
+						</div>
+
+					}
+
+				</div>
+
+				<div className={style.container}>
+					{isLoading ?
+						<div> {textForUser} </div>
+						:
+						<div className={style.cards}>
+							{listOfItems.map((item, index) => (
+								<div key={index}>
+									<Card items={item}/>
+								</div>
+							))}
+						</div>
+					}
+				</div>
 
 				<div className={style.buttons}>
 
-					<button
-						className={style.button}
-						disabled={page <= 1 || allIds.length === 0 || isLoading}
-						onClick={prevPage}
-					> {<ArrowBackIcon/>}
+					<div className={style.buttons}>
 
-					</button>
+						<button
+							className={style.button}
+							disabled={page <= 1 || allIds.length === 0 || isLoading}
+							onClick={prevPage}
+						> {<ArrowBackIcon/>}
 
-					<button
-						disabled={allIds.length === 0 || isLoading}
-						className={style.button}
-						onClick={nextPage}
-					>
-						{<ArrowForwardIcon/>}
-					</button>
+						</button>
+
+						<button
+							disabled={allIds.length === 0 || isLoading}
+							className={style.button}
+							onClick={nextPage}
+						>
+							{<ArrowForwardIcon/>}
+						</button>
+					</div>
+
+
 				</div>
-
-
 			</div>
 		</div>
+
 	);
 };
 
